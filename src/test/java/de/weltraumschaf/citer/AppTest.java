f983cc99904cfe4784bb09edfe57f6cc754741eb
Weltraumschaf <ich@weltraumschaf.de>
2012-02-05 19:02:20 +0100

    Initial commit.

---
+++ src/test/java/de/weltraumschaf/citer/AppTest.java
@@ -0,0 +1,15 @@
+package de.weltraumschaf.citer;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test @Ignore
+    public void testApp() {
+
+    }
+}
