4bee6662d6a12e8d791f6d734c878d8299a4e98a
Weltraumschaf <ich@weltraumschaf.de>
2012-02-05 23:50:40 +0100

    Add simple domain model.

---
+++ src/main/java/de/weltraumschaf/citer/domain/Author.java
@@ -0,0 +1,16 @@
+package de.weltraumschaf.citer.domain;
+
+import java.util.Set;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ * @license http://www.weltraumschaf.de/the-beer-ware-license.txt THE BEER-WARE LICENSE
+ */
+class Author {
+
+    private String id;
+    private String name;
+    private Set<Cite> works;
+
+}
---
+++ src/main/java/de/weltraumschaf/citer/domain/Rating.java
@@ -0,0 +1,13 @@
+package de.weltraumschaf.citer.domain;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ * @license http://www.weltraumschaf.de/the-beer-ware-license.txt THE BEER-WARE LICENSE
+ */
+public class Rating {
+
+    private Cite cite;
+    int stars;
+    
+}
