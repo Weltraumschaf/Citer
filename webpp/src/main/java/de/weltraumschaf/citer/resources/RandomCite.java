154c95164dee522f1c0ad6549f4e42a99e68f91d
Weltraumschaf <ich@weltraumschaf.de>
2012-03-04 00:02:19 +0100

    Use cite repo for random cite.

--- webpp/src/main/java/de/weltraumschaf/citer/resources/RandomCite.java
+++ webpp/src/main/java/de/weltraumschaf/citer/resources/RandomCite.java
@@ -3,9 +3,13 @@ package de.weltraumschaf.citer.resources;
 import de.weltraumschaf.citer.domain.Cite;
 import de.weltraumschaf.citer.domain.Data;
 import de.weltraumschaf.citer.domain.Factory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.codehaus.jettison.json.JSONException;
 
 /**
@@ -13,25 +17,39 @@ import org.codehaus.jettison.json.JSONException;
  * @author Sven Strittmatter <weltraumschaf@googlemail.com>
  * @license http://www.weltraumschaf.de/the-beer-ware-license.txt THE BEER-WARE LICENSE
  */
-public class RandomCite {
+public class RandomCite extends BaseResource {
 
-    private final Data model = Factory.getModel();
+    private final Random randomGenerator = new Random();
+
+    public Cite getRandomCite() {
+        // @todo Return collection instead of iterator.
+        Iterable<Cite> cites = getCiteRepo().getAllCites();
+        List<Cite> l = new ArrayList<Cite>();
+
+        for (Cite c : cites){
+            l.add(c);
+        }
+
+        int randomInt = randomGenerator.nextInt(l.size());
+        return l.get(randomInt);
+    }
 
     @Produces(MediaType.TEXT_HTML)
     @GET public String html() {
-        Cite cite = model.getRandomCite();
-        // @todo convert umls
-        return String.format("<cite>%s</cite> (%s)", cite.getText(), cite.getOriginator().getName());
+        Cite cite = getRandomCite();
+        String text = StringEscapeUtils.escapeHtml4(cite.getText());
+        String name = StringEscapeUtils.escapeHtml4(cite.getOriginator().getName());
+        return String.format("<cite>%s</cite> (%s)", text, name);
     }
 
     @Produces(MediaType.TEXT_PLAIN)
     @GET public String plainText() {
-        Cite cite = model.getRandomCite();
+        Cite cite = getRandomCite();
         return String.format("\"%s\" (%s)", cite.getText(), cite.getOriginator().getName());
     }
 
     @Produces(MediaType.APPLICATION_JSON)
     @GET public Cite json() throws JSONException {
-        return model.getRandomCite();
+        return getRandomCite();
     }
 }
